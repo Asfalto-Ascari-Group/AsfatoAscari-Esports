9d82285c3b71a7c8238460029e4f8505dd1197ca
EXIST <18582414+EXYZED@users.noreply.github.com>
2020-05-16 19:27:02 +0100

    Attempt to do BKG fade.

---
+++ img/design/BackgroundFade.js
@@ -0,0 +1,20 @@
+var bgImageArray = ["YellowGrad@2RES.png", "BlueGrad@2RES.png", "RedGrad@2RES.png"],
+base = "/img/design/",
+secs = 10;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
